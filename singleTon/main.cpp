eef2b21252052e1cd7fe2ca130fbad96876c96fe
surface <15221024927@163.com>
2019-01-07 19:18:01 +0800

    singleTon

--- singleTon/main.cpp
+++ singleTon/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "singleTon.h"
 #include <cstdio>
-int main(int argc, char** argv) {
+int main(int argc, char** argv) {cvcchnzgt;te
 	SingleTon *instance = SingleTon::getInstance();
 	printf("instance:%p\n",instance);
 	instance = SingleTon::getInstance();
