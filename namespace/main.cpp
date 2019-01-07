7b04c9357d9d615ad03a4e7c3c85993453c4933f
surface <15221024927@163.com>
2019-01-07 16:20:46 +0800

    modify namespace

--- namespace/main.cpp
+++ namespace/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include "hello.h"
 
-int main(int argc, char** argv) {	
-	walter::func();
-	graham::func();	
+int main(int argc, char** argv) {
+	int i = 0;
+	for(i = 0;i < 3;++i)
+	{
+		using namespace walter;
+		func();
+	}	
+	using namespace graham;
+	func();	
+	func();
 	return 0;	
 }
