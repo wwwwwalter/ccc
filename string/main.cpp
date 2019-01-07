0d508fe336c7e0ae56ccfd9f431fc94990f503b6
surface <15221024927@163.com>
2019-01-07 17:25:33 +0800

    string

---
+++ string/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv) {
+	string str1;
+	string str2;
+	cout<<"input:"<<endl;
+	cin >> str2>>str1;
+	cout <<str1<<str2<<endl;
+	cout << str1.size()<<endl;
+	cout << (str1==str2)<<endl;
+
+	
+	return 0;
+}
