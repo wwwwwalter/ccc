a58fe196754b01e23e28c31958e6408ceafd005c
surface <15221024927@163.com>
2019-01-07 21:28:23 +0800

    modify 函数模板

--- template/compare.h
+++ template/compare.h
@@ -1,14 +1,15 @@
 #include <cstring>
+//函数模板的声明
 template <typename T>
 bool isequal(T i,T j);
 
-
+//函数模板的实现
 template <typename T>
 bool isequal(T i,T j)
 {
 	return i==j;
 }
-
+//函数模板的特化实现
 template<>
 bool isequal<char*>(char *s1,char *s2)
 {
