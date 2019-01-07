10a8aad85d5612c954f869d07c4d658c7a742579
surface <15221024927@163.com>
2019-01-07 19:17:37 +0800

    modify string

--- string/main.cpp
+++ string/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 int main(int argc, char** argv) {
 	string str1;
@@ -9,6 +10,9 @@ int main(int argc, char** argv) {
 	cout <<str1<<str2<<endl;
 	cout << str1.size()<<endl;
 	cout << (str1==str2)<<endl;
+	cout <<str1.data()<<endl; 
+	printf("%p\n",str1);
+	printf("%p\n",str1.data());
 
 	
 	return 0;
