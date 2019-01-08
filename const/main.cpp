fb92ef07c460944e7c41637d7b347ac793a773c7
surface <15221024927@163.com>
2019-01-08 16:57:23 +0800

    modify const

--- const/main.cpp
+++ const/main.cpp
@@ -6,9 +6,10 @@ int main(void)
 {
     const int a = 10;
     const int &b = a;
+    const int &c = b;
     cout << a <<endl;
     cout << b <<endl;
    // b = 11;//error
-    cout << b << endl;
+    cout << c << endl;
     return 0;
 }
